f9e3ec394989c7878f2ccd30f771e8abb6193ae1
ParthivDataScientist <parthivpatel684@gmail.com>
2026-01-07 16:23:07 +0530

    added the sw.js file in root

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,7 +5,7 @@ class Settings:
     PROJECT_VERSION = os.getenv("PROJECT_VERSION", "1.0.0")
 
     DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./pantry.db")
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY",'')
     ALGORITHM = os.getenv("ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES = int(
         os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
